dc4fbefdd160032f9ffebf404568e772f8459a42
Brunodv <Brunodv>
2024-06-25 19:40:59 -0500

    cambios generales

--- src/Documentos.jsx
+++ src/Documentos.jsx
@@ -2,19 +2,19 @@
 import './documentos.css';
 
 
-function Documentos(){
-    return(
+function Documentos() {
+    return (
         <div className="documentos">
-            <div id='documentosTitle'><h1>Documentos</h1><a className="cv" href="https://drive.google.com/file/d/1DEy5QdhG059wOWUjSMbgvibngxxiL7VW/view?usp=share_link"> ver CV en PDF</a></div>
+            <div id='documentosTitle'><h1>Documentos</h1><a className="cv" href="https://drive.google.com/file/d/1s-Ygb4vpTgB4o7LIAa_aKbA5YSbBTSLb/view?usp=sharing"> ver CV en PDF</a></div>
             <div className="docsColumns">
-             <div className='documento'><h1 className="title">BACHILLER EN INGENIERÍA AERONÁUTICA - UNIVERSIDDAD TECNOLÓGICA DEL PERÚ</h1> <a className='b3' href='https://drive.google.com/file/d/1LeNOX9aUEzOb0i45hJiKgnOpyxWclrtu/view?usp=share_link'>descargar</a></div>
-             <div className='documento'><h1 className="title">DESARROLLO WEB I - 40 HRS. GOOGLE ACTIVATE</h1> <a className='b3' href='https://drive.google.com/file/d/1Vg-mW5V4kPxXy-67a7fy9iDIGmXsgKwG/view?usp=share_link'>descargar</a> </div>
-             <div className='documento'><h1 className="title">DESARROLLO WEB II - 40 HRS. GOOGLE ACTIVATE</h1> <a className='b3' href='https://drive.google.com/file/d/1sNaG1r_3YqPJ-o2H1cy5FquGxQkZyhPL/view?usp=share_link'>descargar</a></div>
-             <div className='documento'><h1 className="title">DATA ANALYSIS WITH PYTHON - COGNITIVE CLASS AND IBM</h1> <a className='b3' href='https://courses.cognitiveclass.ai/certificates/e17620f6b1b940259889be140c0f1e7e'>descargar</a></div>
-             <div className='documento'><h1 className="title">PYTHON 101 FOR DATA SCIENCE - COGNITIVE CLASS AND IBM</h1> <a className='b3' href='https://courses.cognitiveclass.ai/certificates/db9aab3ae3744bee9dac7b9e7f11d417'>descargar</a></div>
-             <div className='documento'><h1 className="title">CIBERSEGURIDAD EN EL TELETRABAJO - GOOGLE ACTIVATE</h1> <a className='b3' href='https://drive.google.com/file/d/1rbdViErOGAnWTWOfUZoSbZvXEAyWJsFD/view?usp=share_link'>descargar</a></div>
-             <div className='documento'><h1 className="title">CERTIFICADO DE PRACTICAS PROFESIONALES DE MANTENIMIENTO AERONÁUTICO - HELICOPTEROS MI 17 - EJERCITO DEL PERÚ</h1> <a className='b3' href='https://drive.google.com/file/d/1AG0mlUuulFaK05Sr8ySsgxwgo6tVuHMe/view?usp=share_link'>descargar</a></div>
-             </div>
+                <div className='documento'><h1 className="title">BACHILLER EN INGENIERÍA AERONÁUTICA - UNIVERSIDDAD TECNOLÓGICA DEL PERÚ</h1> <a className='b3' href='https://drive.google.com/file/d/1LeNOX9aUEzOb0i45hJiKgnOpyxWclrtu/view?usp=share_link'>descargar</a></div>
+                <div className='documento'><h1 className="title">DESARROLLO WEB I - 40 HRS. GOOGLE ACTIVATE</h1> <a className='b3' href='https://drive.google.com/file/d/1Vg-mW5V4kPxXy-67a7fy9iDIGmXsgKwG/view?usp=share_link'>descargar</a> </div>
+                <div className='documento'><h1 className="title">DESARROLLO WEB II - 40 HRS. GOOGLE ACTIVATE</h1> <a className='b3' href='https://drive.google.com/file/d/1sNaG1r_3YqPJ-o2H1cy5FquGxQkZyhPL/view?usp=share_link'>descargar</a></div>
+                <div className='documento'><h1 className="title">DATA ANALYSIS WITH PYTHON - COGNITIVE CLASS AND IBM</h1> <a className='b3' href='https://courses.cognitiveclass.ai/certificates/e17620f6b1b940259889be140c0f1e7e'>descargar</a></div>
+                <div className='documento'><h1 className="title">PYTHON 101 FOR DATA SCIENCE - COGNITIVE CLASS AND IBM</h1> <a className='b3' href='https://courses.cognitiveclass.ai/certificates/db9aab3ae3744bee9dac7b9e7f11d417'>descargar</a></div>
+                <div className='documento'><h1 className="title">CIBERSEGURIDAD EN EL TELETRABAJO - GOOGLE ACTIVATE</h1> <a className='b3' href='https://drive.google.com/file/d/1rbdViErOGAnWTWOfUZoSbZvXEAyWJsFD/view?usp=share_link'>descargar</a></div>
+                <div className='documento'><h1 className="title">CERTIFICADO DE PRACTICAS PROFESIONALES DE MANTENIMIENTO AERONÁUTICO - HELICOPTEROS MI 17 - EJERCITO DEL PERÚ</h1> <a className='b3' href='https://drive.google.com/file/d/1AG0mlUuulFaK05Sr8ySsgxwgo6tVuHMe/view?usp=share_link'>descargar</a></div>
+            </div>
         </div>
     )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -8,22 +8,23 @@ import wtsapp from './img/wtsapp.png';
 
 
 function Home() {
-  
+
   return (
-      <div id="home">
+    <div id="home">
       <div className='homeContainer'>
-        <h1 className='BDVh1'>BRUNO DULANTO VALENTIN</h1>
-      <div className="buttons">
-        <button className='b1'><a className='link' href="https://drive.google.com/file/d/1DEy5QdhG059wOWUjSMbgvibngxxiL7VW/view?usp=share_link"> Descargar CV</a></button>
-         <button className='b1'><a className='link' href="mailto:xcdrxcdr@gmail.com?subject=Queremos%20trabajar%20contigo!&body=Hola%2C%20quisiera%20obtener%20m%C3%A1s%20informaci%C3%B3n%20sobre%20sus%20servicios">Escríbeme</a></button>
+        <h1 className='BDVh1'>Bruno DULANTO VALENTIN</h1>
+        <div className="buttons">
+          <button className='b1'><a className='link' href="https://drive.google.com/file/d/1DEy5QdhG059wOWUjSMbgvibngxxiL7VW/view?usp=share_link"> Descargar CV</a></button>
+          <button className='b1'><a className='link' href="mailto:xcdrxcdr@gmail.com?subject=Queremos%20trabajar%20contigo!&body=Hola%2C%20quisiera%20obtener%20m%C3%A1s%20informaci%C3%B3n%20sobre%20sus%20servicios">Escríbeme</a></button>
+        </div>
+
+        <div className='socials'>
+          <button className='b2'><a href="https://github.com/Brunodv"><img src={github} alt="github" /></a></button>
+          <button className='b2'><a href="https://www.instagram.com/brunodulantovalentin/"><img src={inst} alt="instagram" /></a></button>
+          <button className='b2'><a href="https://wa.me/51977631700"><img src={wtsapp} alt="whatsapp" /></a></button>
+        </div>
       </div>
-      
-      <div className='socials'>
-        <button className='b2'><a href="https://github.com/Brunodv"><img src={github} alt="github" /></a></button>
-        <button className='b2'><a href="https://www.instagram.com/brunodulantovalentin/"><img src={inst} alt="instagram" /></a></button>
-        <button className='b2'><a href="https://wa.me/51977631700"><img src={wtsapp} alt="whatsapp" /></a></button>
-      </div>        
-      </div>
-      </div>
-  )}
-  export default Home;
+    </div>
+  )
+}
+export default Home;
